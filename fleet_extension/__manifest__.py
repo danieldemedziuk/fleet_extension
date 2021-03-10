a1d5a728bc86ac0998a24d4c6a33904f4ea53b3b
danieldemedziuk <daniel.demedziuk@gmail.com>
2021-03-10 13:58:19 +0100

    New description

--- fleet_extension/__manifest__.py
+++ fleet_extension/__manifest__.py
@@ -12,51 +12,25 @@
     'price':   9,
     'currency':   'EUR',
     'description': """
-    # Odoo12 Fleet extension
-## Table of contents
-* [General info](#general-info)
-* [Features](#features)
-* [Technical](#technical)
-
-## General info
-This add-on to the Fleet module extends the basic module with additional data and makes changes to contracts and odometer.
-
-<img src="https://i.ibb.co/yyJb1kS/Icon.png" alt="Form" border="0">
-
-### Features:
-- date of registration inspection
-- car equipment
-- tire type
-- vignette
-- insurance
-and others.
-Additionally, it creates potential contracts with selected brands in order to facilitate work organization.
-
-## Technical
-Technical name: <b>fleet_extension</b>
-### Depends:
-- fleet
-
-The odometer system was extended by additional fields. The purpose of this extension is to more precisely determine kilometers driven on a given trip, day or month.
-Updates to the module's current status are planned. In the future the module is to provide more beneficial services to the user.
-
-<img src="https://i.ibb.co/bWGTpKq/form.png" alt="Icon" border="0">
-
-
 Fleet Module Extension
 ==================================
 This addition to the Fleet module extends the basic module with additional data such as:
-- date of registration inspection
-- car equipment
-- tire type
-- vignette
-- insurance
+
+Main Features
+-------------
+* date of registration inspection
+* car equipment
+* tire type
+* vignette
+* insurance
 and others.
 
+Depends
+---------
+* fleet
+
 Additionally, it creates potential contracts with selected brands in order to facilitate work organization.
-
 The odometer system was extended by additional fields. The purpose of this extension is to more precisely determine kilometers driven on a given trip, day or month.
-
 Updates to the module's current status are planned. In the future the module is to provide more beneficial services to the user.
 
 """,
